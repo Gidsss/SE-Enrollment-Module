6270729a4572477b9e33d488b30b47e71222cfcd
Gidsss <88811389+Gidsss@users.noreply.github.com>
2023-11-21 18:22:12 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,5 @@
 <script src="{{ url('public/backend/dist/js/adminlte.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ url('public/backend/dist/js/demo.js') }} "></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ url('public/backend/dist/js/pages/dashboard.js') }}"></script>
 </body>
 </html>

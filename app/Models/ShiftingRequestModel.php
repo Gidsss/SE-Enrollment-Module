a952dabc0a81d7a88ecdd07d0affdec96e273fc2
HolyShaq <acebyronehalili@gmail.com>
2024-05-05 15:50:28 +0800

    renamed models

--- app/Models/ShiftingRequestModel.php
+++ app/Models/ShiftingRequestModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ShiftingRequest extends Model
+class ShiftingRequestModel extends Model
 {
     use HasFactory;
     protected $fillable = [

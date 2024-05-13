a6927f6a8865e42d930d2020f619cb8dda565a4d
Gidsss <88811389+Gidsss@users.noreply.github.com>
2024-05-13 23:10:29 +0800

    Update SelectProgram.php
    Dropdown content will include all programs except the current program of student

--- app/Livewire/SelectProgram.php
+++ app/Livewire/SelectProgram.php
@@ -5,8 +5,6 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\ShiftingRequest;
 use App\Models\DegreeProgram;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class SelectProgram extends Component
@@ -21,15 +19,19 @@ class SelectProgram extends Component
     {
         $student = Auth::guard('student')->user();
         $this->student_id = $student->student_id;
-        $this->programs = DegreeProgram::all();
+        $currentProgram = $student->degree_program;  // Fetch the current program of the student (this is the name of the column in the students table as I've defined in model)
+
+        // Fetch all programs except the current one
+        $this->programs = DegreeProgram::where('program_name', '!=', $currentProgram)->get();
 
         $existingRequest = ShiftingRequest::where('student_id', $this->student_id)->first();
         if ($existingRequest && $existingRequest->is_finalized) {
             $this->selectedProgram = $existingRequest->new_degree_program;
             $this->message = 'You have already finalized your program selection.';
-            $this->isDisabled = true; // Disable if the request is finalized
+            $this->isDisabled = true;
         }
     }
+
     public function saveNewProgram()
     {
         $shiftingRequest = ShiftingRequest::firstOrNew([
@@ -37,12 +39,12 @@ class SelectProgram extends Component
         ]);
 
         $shiftingRequest->new_degree_program = $this->selectedProgram;
-        $shiftingRequest->status = 'Initial'; // Set an initial status or similar
+        $shiftingRequest->status = 'Initial'; // Set an initial status
         $shiftingRequest->save();
 
         $this->message = 'Program successfully selected.';
-
     }
+
     public function render()
     {
         return view('livewire.select-program');

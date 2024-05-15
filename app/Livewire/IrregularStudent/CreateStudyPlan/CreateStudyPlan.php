6c2b1a947faf204df3ce48d82bd8059ac9e60e42
migo1311 <josephmiguel326@gmail.com>
2024-05-16 00:04:31 +0800

    dynamic-docu-status

--- app/Livewire/IrregularStudent/CreateStudyPlan/CreateStudyPlan.php
+++ app/Livewire/IrregularStudent/CreateStudyPlan/CreateStudyPlan.php
@@ -4,26 +4,43 @@ namespace App\Livewire\IrregularStudent\CreateStudyPlan;
 
 use Livewire\Component;
 use App\Models\StudyPlanValidations;
+use Illuminate\Support\Facades\Auth;
 
 class CreateStudyPlan extends Component
 {  
+    public $studentId;
     public $Status;
+    public $hasPending = false;
+    public $hasReject = false;
+    public $hasApprove = false;
 
     public function mount()
     {
-        $this->student_id = '202120095';
-        $study_plan_validation = StudyPlanValidations::where('student_id', $this->student_id)->first();
+        $this->studentId = Auth::guard('student')->user()->student_id;
+        $study_plan_validation = StudyPlanValidations::where('student_id', $this->studentId)->first();
         if ($study_plan_validation) {
             $this->Status = $study_plan_validation->status;
+            $this->setStatusFlags();
+        }
+    }
+
+    public function setStatusFlags()
+    {
+        if ($this->Status === 'Pending') {
+            $this->hasPending = true;
+        } elseif ($this->Status === 'Revise') {
+            $this->hasReject = true;
+        } elseif ($this->Status === 'Approved') {
+            $this->hasApprove = true;
         }
     }
 
     public function render()
     {
         return view('livewire.irregular-student.create-study-plan.create-study-plan', [
-            'hasPending' => $this->Status === 'Pending',
-            'hasReject' => $this->Status === 'Revise',
-            'hasApprove' => $this->Status === 'Approved',
+            'hasPending' => $this->hasPending,
+            'hasReject' => $this->hasReject,
+            'hasApprove' => $this->hasApprove,
         ])->layout('livewire.irregular-student.irregular-app');
     }
 }
